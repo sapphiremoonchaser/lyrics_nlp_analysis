86ec0b51189fc1818ab0d1ca4644cdee4200e322
violetdiamonds <violetdiamonds@protonmail.com>
2026-09-10 13:13:53 -0500

    add taylor color palettes

--- src/lyrics_nlp/dashboard/visualizations/color_palettes.py
+++ src/lyrics_nlp/dashboard/visualizations/color_palettes.py
@@ -1,70 +1,139 @@
-"""Color palettes for Lana albums"""
+"""Color palettes foralbums"""
 
 album_palettes = {
-    "Sirens": [
-        "#87CEEB",
-        "#4682B4",
-        "#DEB887",
-        "#8B4513",
-    ],
-    "Lana Del Ray, A.K.A. Lizzy Grant": [
-        "#DAA520",
-        "#000000",
-        "#FFDAB9",
-        "#8B4513",
-    ],
-    "Born To Die": [
-        "#4169E1",
-        "#ADD8E6",
-        "#B22222",
-        "#FFB6C1",
-    ],
-    "Paradise": [
-        "#006600",
-        "#228B22",
-        "#DAA520",
-        "#000000",
-    ],
-    "Ultraviolence": [
-        "#663399",
-        "#000000",
-        "#505050",
-        "#808080",
-    ],
-    "Honeymoon": [
-        "#800000",
-        "#FFB6C1",
-        "#99ccff",
-        "#000000",
-    ],
-    "Lust for Life": [
-        "#FFD700",
-        "#800000",
-        "#ff4d4d",
-        "#DEB887",
-    ],
-    "Norman Fucking Rockwell!": [
-        "#4682B4",
-        "#B0C4DE",
-        "#696969",
-        "#66ff66",
-    ],
-    "Chemtrails Over the Country Club": [
-        "#303030",
-        "#888888",
-        "#FFB6C1",
-        "#e6ccff",
-    ],
-    "Blue Banisters": [
-        "#4682B4",
-        "#8B4513",
-        "#87CEEB",
-        "#cc9966",
-    ],
-    "Did You Know That There's a Tunnel Under Ocean Blvd": [
-        "#303030",
-        "#707070",
-        "#ffff66",
-        "#ffccff",
-    ],
+    "Lana Del Rey": {
+        "Sirens": [
+            "#87CEEB",
+            "#4682B4",
+            "#DEB887",
+            "#8B4513",
+        ],
+        "Lana Del Ray, A.K.A. Lizzy Grant": [
+            "#DAA520",
+            "#000000",
+            "#FFDAB9",
+            "#8B4513",
+        ],
+        "Born To Die": [
+            "#4169E1",
+            "#ADD8E6",
+            "#B22222",
+            "#FFB6C1",
+        ],
+        "Paradise": [
+            "#006600",
+            "#228B22",
+            "#DAA520",
+            "#000000",
+        ],
+        "Ultraviolence": [
+            "#663399",
+            "#000000",
+            "#505050",
+            "#808080",
+        ],
+        "Honeymoon": [
+            "#800000",
+            "#FFB6C1",
+            "#99ccff",
+            "#000000",
+        ],
+        "Lust for Life": [
+            "#FFD700",
+            "#800000",
+            "#ff4d4d",
+            "#DEB887",
+        ],
+        "Norman Fucking Rockwell!": [
+            "#4682B4",
+            "#B0C4DE",
+            "#696969",
+            "#66ff66",
+        ],
+        "Chemtrails Over the Country Club": [
+            "#303030",
+            "#888888",
+            "#FFB6C1",
+            "#e6ccff",
+        ],
+        "Blue Banisters": [
+            "#4682B4",
+            "#8B4513",
+            "#87CEEB",
+            "#cc9966",
+        ],
+        "Did You Know That There's a Tunnel Under Ocean Blvd": [
+            "#303030",
+            "#707070",
+            "#ffff66",
+            "#ffccff",
+        ],
+    },
+
+    "Taylor Swift":{
+        "Taylor Swift": [
+            "#FFD700",
+            "#008080",
+            "#228B22",
+            "#20B2AA"
+        ],
+        "Fearless": [
+           "#8B4513",
+            "#B8860B",
+            "#DEB887"
+        ],
+        "Speak Now": [
+            "#DEB887",
+            "#862d86",
+            "#d98cd9",
+            "#ffd9b3"
+        ],
+        "Red": [
+            "#990000",
+            "#cc5200",
+            "#ffd633"
+        ],
+        "1989": [
+            "#000099",
+            "#4d4dff",
+            "#b3b3ff"
+        ],
+        "Reputation": [
+            "#0d0d0d",
+            "#0d0d0d",
+            "#a6a6a6"
+        ],
+        "Lover": [
+            "#ff99ff",
+            "#ff99ff",
+            "#80bfff",
+            "#ffff33"
+        ],
+        "Folklore": [
+            "#0d0d0d",
+            "#0d0d0d",
+            "#a6a6a6"
+        ],
+        "Evermore": [
+            "#990000",
+            "#cc5200",
+            "#ffd633"
+        ],
+        "Midnights": [
+            "#000066",
+            "#80bfff",
+            "#8c1aff"
+        ],
+        "The Tortured Poets Department": [
+            "#0d0d0d",
+            "#0d0d0d",
+            "#a6a6a6"
+        ],
+        "The Life of a Showgirl": [
+            "#008060",
+            "#00e6b8",
+            "#ff8533"
+        ]
+    }
+
 }
